fbbd7430a6e032003aeedcd2b5b836d8b65c2ed2
Jithu-9847 <131839320+Jithu-9847@users.noreply.github.com>
2023-05-24 11:08:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById("card1").addEventListener("mouseover",()=>{
+
+    document.getElementById("car1").style.display="block";
+    document.getElementById("about").style.top=" ";
+    
+
+
+});
+document.getElementById("card1").addEventListener("mouseout",()=>{
+
+    document.getElementById("car1").style.display="none";
+    document.getElementById("about").style.top="120%";
+
+});
+document.getElementById("card2").addEventListener("mouseover",()=>{
+
+    document.getElementById("car2").style.display="block";
+    document.getElementById("about").style.top=" %";
+
+});
+document.getElementById("card2").addEventListener("mouseout",()=>{
+
+    document.getElementById("car2").style.display="none";
+    document.getElementById("about").style.top="120%";
+
+});
+document.getElementById("card3").addEventListener("mouseover",()=>{
+
+    document.getElementById("car3").style.display="block";
+    document.getElementById("about").style.top=" ";
+
+});
+document.getElementById("card3").addEventListener("mouseout",()=>{
+
+    document.getElementById("car3").style.display="none";
+    document.getElementById("about").style.top="120%";
+
+});
+document.getElementById("drop").addEventListener("click",()=>{
+
+    document.getElementById("drop_menu").style.display="block";
+    
+
+});
